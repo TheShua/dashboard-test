eaf64241bd951d7b5bb8d18d0b0df987dbd4163f
Shua <g.deglisezinski@gmail.com>
2021-01-28 18:29:10 +0100

    Correct the configuration

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const config = {
     timerMin: 1,            // Timer minimum for generate diagnostic
-    timerMax: 6000          // Timer maximum for generate diagnostic
+    timerMax: 12000          // Timer maximum for generate diagnostic
 }
